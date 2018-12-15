fd249290dd10d5835d66fbd803ffaf5033f011bd
Kolkies <kolkies@kolkiesdev.nl>
2018-12-15 18:34:41 +0100

    Working on music

--- commands/Music/add.js
+++ commands/Music/add.js
@@ -13,10 +13,10 @@ class PlayCommand extends Command {
 	}
 
 	async run(msg, song) {
-    const { valid, id, type } = this.fetchFromURL(song);
-    if (valid) return await this.addSong(msg, id, type);
+		const { valid, id, type } = this.fetchFromURL(song);
+		if (valid) return await this.addSong(msg, id, type);
 
-    const searchResult = await msg.guild.music.search(song, type);
+		const searchResult = await msg.guild.music.search(song, type);
 		const songChoices = ['Please type the number of the song you want to be played (1-5). Type `cancel` to exit adding the song.', ''];
 
 		for (let i = 0; i < searchResult.length; i++) {
@@ -38,16 +38,15 @@ class PlayCommand extends Command {
 			if (choices.first().content.toLowerCase() === 'cancel') return await msg.send('Adding song discarded...');
 			const choice = parseInt(choices.first().content.toLowerCase());
 			const song = await this.addSong(msg, searchResult[choice - 1].id);
-			if (msg.guild.music.status == "paused" || msg.guild.music.status == "playing") return msg.send(`I added song **${song.title}** to the queue!`);
+			if(msg.guild.music.status == "paused" || msg.guild.music.status == "playing") return msg.send(`I added song **${song.title}** to the queue!`);
 			await msg.guild.music.play();
 			return msg.send(`I am now going to play ${song.title}`);
 		} catch (err) {
 			console.log(err);
 			return await msg.send('Not choosen');
 		}
-
 	}
-	
+
 	async addSong(msg, song, type) {
 		const addedSong = await msg.guild.music.add(msg.author, song, { force: true, type });
 		return addedSong;
