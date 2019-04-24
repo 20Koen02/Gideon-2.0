72d0c6e05a086fa4073a6774b2a998a781c7f78a
20Koen02 <kvanwijn@online.nl>
2019-04-24 13:19:30 +0200

    cat command

--- commands/Fun/cat.js
+++ commands/Fun/cat.js
@@ -1,12 +1,12 @@
 const { Command } = require("klasa");
 const request = require("request");
 
-const api = "https://dog.ceo/api/breeds/image/random";
+const api = "http://shibe.online/api/cats";
 
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
-            description: "Gets a random image from a dog"
+            description: "Gets a random image from a cat"
         });
     }
 
@@ -19,13 +19,13 @@ module.exports = class extends Command {
             function(error, response, body) {
                 if (response.statusCode === 503) return message.channel.send("503: Service Unavailable");
                 if (!error && response.statusCode === 200) {
-                    const dogEmbed = message.client.helpers.Miscs.getEmbed({
+                    const catEmbed = message.client.helpers.Miscs.getEmbed({
                         color: message.guild.settings.appearance.embedcolor,
-                        text: "Powered by: https://dog.ceo/",
+                        text: "Powered by: http://shibe.online/",
                         footer: true
                     });
-                    dogEmbed.setTitle(":dog: Dog").setImage(body.message);
-                    message.channel.send({ embed: dogEmbed });
+                    catEmbed.setTitle(":cat: Cat").setImage(body[0]);
+                    message.channel.send({ embed: catEmbed });
                 }
             }
         );
