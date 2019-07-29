1e36c515de67801b6b292d63cf15ce5b8549a66c
KolkiesDev <stijn.vdkolk@icloud.com>
2019-07-29 13:52:17 +0200

    Fix url shortener errors

--- src/lib/URLShortener/index.ts
+++ src/lib/URLShortener/index.ts
@@ -1,21 +1,7 @@
-import * as https from "https";
+import fetch from "node-fetch";
 
-export const shorten = (url: string) : Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const options = `https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`;
+export const shorten = async (url:string) => {
 
-    https.get(options, (response) => {
-      if (response.statusCode >= 400) {
-        reject(new Error(`${response.statusCode} ${response.statusMessage}`));
-      }
-
-      response.on('data', (data) => {
-        // TinyUrl API support HTTPS short link generation but not their API
-        // so we need to replace the protocol.. :(
-        resolve(data.toString().replace('http://', 'https://'));
-      });
-    }).on('error', (error) => {
-      reject(error);
-    });
-  });
+  const res = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+  return res.text();
 };
