12d08807317733f07e55fe015b3020c3eed39068
KolkiesDev <stijn.vdkolk@icloud.com>
2019-07-29 13:50:06 +0200

    js --> ts

---
+++ src/commands/Fun/dice.ts
@@ -0,0 +1,18 @@
+import { applyOptions } from "../../lib/Util/Util";
+import { CommandOptions, Command, KlasaMessage } from "klasa";
+import { MessageAttachment } from "discord.js";
+import { readFile } from "fs-nextra";
+import { Canvas } from "canvas-constructor";
+import { DiceImage } from "../../lib/GideonImageGenerator/GideonImages";
+
+@applyOptions<CommandOptions>({
+  cooldown: 5,
+  description: "Rolls dices"
+})
+
+export default class DiceCommand extends Command {
+
+  async run(message: KlasaMessage) {
+    return message.send(new MessageAttachment(await new DiceImage(message).getImage(Math.floor(Math.random() * 6) + 1), "dice.png"));
+  }
+};
---
+++ src/commands/Fun/dog.ts
@@ -0,0 +1,21 @@
+import { applyOptions } from "../../lib/Util/Util";
+import { CommandOptions, Command } from "klasa";
+import { KlasaMessage } from "klasa";
+import { Embed } from "../../lib/Embed";
+import fetch from "node-fetch";
+
+@applyOptions<CommandOptions>({
+  description: "Gets a random image from a dog"
+})
+
+export default class DogCommand extends Command {
+  async run(message:KlasaMessage) {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const json = await res.json();
+    if(res.status === 503) return message.send("503: Service Unavailable");
+    const embed = Embed(message, { footer: true, text: "Powered by: https://dog.ceo" })
+      .setTitle(":dog: Dog")
+      .setImage(json.message);
+    return message.sendEmbed(embed);
+    }
+};
