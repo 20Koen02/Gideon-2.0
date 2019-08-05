5e3277a90337adbf5b7da0b51d621feb40f742e2
KolkiesDev <stijn.vdkolk@icloud.com>
2019-08-05 11:11:31 +0200

    Add queue and skip command

---
+++ src/commands/Music/queue.ts
@@ -0,0 +1,22 @@
+import { applyOptions } from "@lib/Util/Util";
+import { CommandOptions, Command, KlasaMessage } from "klasa";
+import Music from "@lib/GideonMusic/Music";
+import { GideonEmbedManager } from "@lib/GideonEmbedManager/GideonEmbedManager";
+
+
+@applyOptions<CommandOptions>({
+  description: "Queue music",
+  runIn: ["text"]
+})
+
+export default class QueueMusicCommand extends Command {
+
+	async run(message:KlasaMessage) {
+    const { music } = message.guild;
+    if (!music.playing) return message.translate("cmd_music_notplaying");
+
+    if (music.queue.length < 1) return message.send("There is no music in the queue");
+    
+    return message.sendEmbed(GideonEmbedManager.music().queueEmbed(message, music.queue));
+  }
+};
